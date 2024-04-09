491c937aeae4601293d0595a19c5328dff8d5bbe
Muhammadtanzeelkhan <161112644+Muhammadtanzeelkhan@users.noreply.github.com>
2024-04-09 03:20:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+"use strict";
+// Q2:
+// let personName = "Tanzeel"
+// console.log("Hello",personName,"would you like to play football");
+//Q3:
+// let personName="Tanzeel";
+// console.log(personName.toLowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.charAt(0).toUpperCase()+ personName.slice(1).toUpperCase());
+//Q4:
+// console.log("Albert Einstein once siad","word IMPOSSIBLE itself says I AM possible");
+//Q5:
+// let famousperson="Albert Einstein";
+// let message="word IMPOSSIBLE itself says I AM possible";
+// console.log(famousperson,message);
+//Q6:
+// let whiteSpace="\n\t Tanzeel\t\n";
+// console.log(whiteSpace);
+// let withoutwhiteSpace = whiteSpace.trim();
+// console.log(withoutwhiteSpace);
+//Q7,Q8:
+// console.log(4+4);
+// console.log(10-2);
+// console.log(2*4);
+// console.log(16/2);
+//Q9:
+// let favouriteNumber=10;
+// console.log("my favourite number is", favouriteNumber);
+//Q10:
+// let whiteSpace="\n\t Tanzeel\t\n";//n is used for new line and \t is used for whitespace
+// console.log(whiteSpace);
+// let withoutwhiteSpace = whiteSpace.trim();
+// console.log(withoutwhiteSpace);
+//Q11:
+// let friendName =["Taha","Ayan","Ali","Arham"]
+// console.log(friendName[0])
+// console.log(friendName[1])
+// console.log(friendName[2])
+// console.log(friendName[3])
+//Q12:
+// let friendName =["Taha","Ayan","Ali","Arham"]
+// let message="You are my best friend"
+// console.log("Hello",friendName[0],message)
+// console.log("Hello",friendName[1],message)
+// console.log("Hello",friendName[2],message)
+// console.log("Hello",friendName[3],message)
+// Q13:
+// let transpotstion = ["Civic","Helux","City","Audietron",];
+// transpotstion.map((items) => console.log(`I would like to own ${items}`));
+// Q14:
+// let guestArr = ["Taha","Ayan","Ali","Arham"];
+// guestArr.map((items) => console.log(`Dear ${items} you are invited to the dinner `)); 
+// Q15:
+// let guestArr = ["Taha","Ayan","Ali","Arham"];
+// let canNotAttend = "Arham"
+/// console.log(canNotAttend + "can not attend the dinner")
+// let newGuest = "Hadi"
+// guestArr [guestArr.indexOf(canNotAttend)] = newGuest;
+// console.log(guestArr) 
+// guestArr.map((items) => console.log(`Dear ${items} you are invited to the dinner `)); 
